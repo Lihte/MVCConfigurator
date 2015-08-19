637dc0f6de7a7caaa50294667f0c376b62778886
fboethius <fredrik9a@hotmail.com>
2015-08-19 17:24:33 +0200

    Added Product

---
+++ MVCConfigurator.Domain/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCConfigurator.Domain.Models
+    {
+    public class Product:EntityBase
+        {
+        public string Name { get; set; }
+        public string ProductCode { get; set; }
+        public string ImagePath { get; set; }
+        public List<List<Part>> Parts { get; set; }
+        public Category Category { get; set; }
+
+        }
+    }

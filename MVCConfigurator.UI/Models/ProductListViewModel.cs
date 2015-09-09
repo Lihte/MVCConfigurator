4d2fa7d6eb4af5f61e89c8f2d3a9a87bbe73409e
Tomas Hjelm <ful_gnagare@hotmail.com>
2015-09-09 17:58:14 +0200

    Build adminproductlist

---
+++ MVCConfigurator.UI/Models/ProductListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MVCConfigurator.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCConfigurator.UI.Models
+{
+    public class ProductListViewModel
+    {
+        public IList<Product> Products { get; set; }
+    }
+}

3902a0c0bcbf5112a88f006ff554c3f90c49d3f4
fboethius <fredrik9a@hotmail.com>
2015-09-21 14:19:17 +0200

    Added bundling/minification

---
+++ MVCConfigurator.UI/App_Start/BundleConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using Yahoo.Yui.Compressor;
+
+namespace MVCConfigurator
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/js")
+                .Include("~/Scripts/modernizr-{version}.js",new YuiCompressor())
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/bootstrap.js"));
+                
+            
+
+            bundles.Add(new StyleBundle("~/bundles/styles")
+                .Include("~/Content/bootstrap.css")
+                .Include("~/Content/Site.css")
+                
+                );
+                
+        }
+    }
+    public class YuiCompressor : IItemTransform
+    {
+
+        public string Process(string includedVirtualPath, string input)
+        {
+            return new JavaScriptCompressor().Compress(input);
+        }
+    }
+}
